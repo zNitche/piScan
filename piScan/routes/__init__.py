d0a253db3bfe4d7d8f018339a62cacd8424fbaf2
zNitche <53448802+zNitche@users.noreply.github.com>
2024-01-30 00:08:30 +0100

    routes structure tweaks

--- piScan/routes/__init__.py
+++ piScan/routes/__init__.py
@@ -1,11 +1,6 @@
+from piScan.routes import app
+
 from piScan.routes.core import core
 from piScan.routes.api import api
 from piScan.routes.docs import docs
 
-from piScan import db
-from flask import current_app
-
-
-@current_app.teardown_appcontext
-def teardown_appcontext(exception=None):
-    db.close_session(exception=exception)
