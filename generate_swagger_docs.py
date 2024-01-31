863a860a429c5ec885232bac241d05aa7cc4e2b7
zNitche <53448802+zNitche@users.noreply.github.com>
2024-02-01 00:06:47 +0100

    output log for swagger.json generation script

--- generate_swagger_docs.py
+++ generate_swagger_docs.py
@@ -4,9 +4,12 @@ import json
 
 
 def main():
+    print("Generating swagger.json...")
     with open(Config.SWAGGER_SCHEMA_PATH, "w") as file:
         json_specs = json.dumps(spec.to_dict(), indent=2)
         file.write(json_specs)
+
+    print(f"Done... Saved to: {Config.SWAGGER_SCHEMA_PATH}")
 
 
 if __name__ == '__main__':
