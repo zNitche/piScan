a7aa56cf6d553ea59f86e0e70434da7f803f081e
zNitche <53448802+zNitche@users.noreply.github.com>
2024-02-02 17:42:55 +0100

    updated docs for devices routes

--- api_docs/routes/api/devices.py
+++ api_docs/routes/api/devices.py
@@ -1,21 +1,107 @@
 from api_docs.spec import spec
 
-spec.path(
-    path="/api/devices",
-    operations=dict(
-        get=dict(
-            responses={
-                "200": {
-                    "content": {
-                        "application/json": {
-                            "schema": {
-                                "type": "array",
-                                "items": "Device"
-                            }
-                        }
+
+spec.tag({"name": "Devices"})
+
+
+get_devices = {
+    "tags": ["Devices"],
+    "summary": "Get Devices",
+    "responses": {
+        "200": {
+            "content": {
+                "application/json": {
+                    "schema": {
+                        "type": "array",
+                        "items": "Device"
                     }
                 }
             }
-        )
-    ),
+        }
+    }
+}
+
+create_device = {
+    "tags": ["Devices"],
+    "summary": "Create device",
+    "requestBody": {
+        "content": {
+            "application/json": {
+                "schema": "Device"
+            }
+        }
+    },
+    "responses": {
+        "201": {
+            "description": "device successfully created"
+        },
+        "400": {
+            "description": "error while creating device"
+        }
+    }
+}
+
+
+spec.path(
+    path="/api/devices",
+    operations={
+        "get": get_devices,
+        "post": create_device,
+    },
+)
+
+get_device = {
+    "tags": ["Devices"],
+    "summary": "Get device by uuid",
+    "parameters": [
+        {
+            "in": "path",
+            "name": "uuid",
+            "schema": {
+                "type": "string"
+            }
+        }
+    ],
+    "responses": {
+        "200": {
+            "content": {
+                "application/json": {
+                    "schema": "Device"
+                }
+            }
+        },
+        "404": {
+            "description": "NOT FOUND"
+        }
+    }
+}
+
+remove_device = {
+    "tags": ["Devices"],
+    "summary": "Delete device by uuid",
+    "parameters": [
+        {
+            "in": "path",
+            "name": "uuid",
+            "schema": {
+                "type": "string"
+            }
+        }
+    ],
+    "responses": {
+        "200": {
+            "description": "Device removed successfully"
+        },
+        "404": {
+            "description": "NOT FOUND"
+        }
+    }
+}
+
+spec.path(
+    path="/api/devices/{uuid}",
+    operations={
+        "get": get_device,
+        "delete": remove_device,
+    },
 )
--- api_docs/spec.py
+++ api_docs/spec.py
@@ -7,8 +7,6 @@ spec = APISpec(
     version="1.0.0",
     openapi_version="3.0.2",
     info={"description": "API for piScan system"},
-    options={
-        "servers": [{"url": "http://127.0.0.1", "name": "localhost"}]
-    },
+    servers=[{"url": "http://127.0.0.1:8000"}],
     plugins=[MarshmallowPlugin()]
 )
