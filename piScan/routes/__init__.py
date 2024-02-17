f0f71d61da79b6f536f801ad0b5e07941e8d6f83
zNitche <53448802+zNitche@users.noreply.github.com>
2024-02-17 21:56:18 +0100

    tweaked app related routes

--- piScan/routes/__init__.py
+++ piScan/routes/__init__.py
@@ -1,5 +1,19 @@
-from piScan.routes import app
+from piScan import db
+from flask import current_app
 
 from piScan.routes import core
 from piScan.routes import api
 from piScan.routes import docs
+
+
+@current_app.after_request
+def after_request(response):
+    # meant to be LAN only so we don't have to care about strict cors
+    response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return response
+
+
+@current_app.teardown_appcontext
+def teardown_appcontext(exception=None):
+    db.close_session(exception=exception)
